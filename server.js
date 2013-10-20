71a5d19ddc34b5f4ec15437defad4c24483fd1b4
Todd Branchflower <todd.branchflower@gmail.com>
2013-10-20 10:28:49 -0600

    added support to emit number of players on connect and disconnect

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ var io = require('socket.io').listen(8080)
     , sockets = [];
 
 io.sockets.on('connection', function (socket) {
-    sockets.push(socket);
-
-  socket.emit('message', "connected");
 
   socket.on('message', function (data) {
       console.log(data);
       emitToOtherSockets(data, socket);
   });
+
+  socket.on('disconnect', function () {
+      console.log('disconnect!');
+      removeSocketFromArray(socket);
+      updateNumberOfPlayers(sockets[0]);
+  });
+
+    sockets.push(socket);
+    updateNumberOfPlayers(socket);
 });
+
+function updateNumberOfPlayers(socket)
+{
+    var playerObj = {"players": sockets.length};
+    socket.emit('message', playerObj);
+    emitToOtherSockets(playerObj, socket);
+}
 
 function emitToOtherSockets(data, emittingSocket)
 {
@@ -18,5 +31,18 @@ function emitToOtherSockets(data, emittingSocket)
     {
         if (sockets[socket] != emittingSocket)
             sockets[socket].emit('message', data);
+    }
+}
+
+function removeSocketFromArray(disconnectingSocket)
+{
+    for (var i in sockets)
+    {
+        if (sockets[i] === disconnectingSocket)
+        {
+            // remove element
+            sockets.splice(i, 1);
+            console.log("removed!");
+        }
     }
 }
